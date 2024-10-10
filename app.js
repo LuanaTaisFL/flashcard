771775d7553264108ecda30ef56c2dac214a99a5
Luana Tais de Freitas de Lima <107936487+LuanaTaisFL@users.noreply.github.com>
2024-10-10 10:48:08 -0300

    3

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ function criaCartao(categoria, pergunta, resposta ) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
-    cartao.innerHTML = 
-    <div class="cartao-conteudo">
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
     <h3>${categoria}</h3>
-    <div class="cartao_conteudo_pergunta">
+    <div class="cartao__conteudo__pergunta">
         <p>${pergunta}</p>
     </div>
-    <div class="cartao_conteudo_resposta">
+    <div class="cartao__conteudo__resposta">
         <p>${resposta}</p>
     </div>
     </div>
-
+`
 
     let respostaEstaVisivel = false
 
